a8a85b30d4d342cbdc04e585b4544207116e8217
Ben Thomasson <bthomass@redhat.com>
2022-02-01 16:21:44 -0500

    Adds remote tests

---
+++ test_ftl_remote.py
@@ -0,0 +1,55 @@
+
+
+from faster_than_light import run_module, load_inventory
+import pytest
+from pprint import pprint
+
+
+@pytest.mark.asyncio
+async def test_argtest():
+    result = await run_module(load_inventory('inventory2.yml'), ['modules'], 'argtest', module_args=dict(somekey='somevalue'))
+    pprint(result)
+    assert result is not None
+    assert 'localhost' in result
+    assert result['localhost']['args']
+    assert result['localhost']['more_args'] == 'somekey=somevalue'
+    assert result['localhost']['files']
+    assert result['localhost']['executable']
+    assert result['localhost']['env']
+
+
+@pytest.mark.asyncio
+async def test_argtestjson():
+    result = await run_module(load_inventory('inventory2.yml'), ['modules'], 'argtestjson', module_args=dict(somekey='somevalue'))
+    pprint(result)
+    assert result is not None
+    assert 'localhost' in result
+    assert result['localhost']['args']
+    assert result['localhost']['more_args'] == '{"somekey": "somevalue"}'
+    assert result['localhost']['files']
+    assert result['localhost']['executable']
+    assert result['localhost']['env']
+
+
+@pytest.mark.asyncio
+async def test_timetest():
+    result = await run_module(load_inventory('inventory2.yml'), ['modules'], 'timetest')
+    assert result is not None
+    assert 'localhost' in result
+    assert 'time' in result['localhost']
+
+
+@pytest.mark.asyncio
+async def test_my_test_info():
+    result = await run_module(load_inventory('inventory2.yml'), ['modules'], 'my_test_info', module_args=dict(name='foo'))
+    assert result is not None
+    assert 'localhost' in result
+    assert 'original_message' in result['localhost']
+
+
+@pytest.mark.asyncio
+async def test_new_style_module():
+    result = await run_module(load_inventory('inventory2.yml'), ['modules'], 'new_style_module', module_args=dict(name='foo'))
+    assert result is not None
+    assert 'localhost' in result
+    assert 'original_message' in result['localhost']
